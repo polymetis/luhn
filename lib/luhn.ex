4350ebe57cd454543af6df929212da97349a4869
Ley Missailidis <moisis.langley@missailidis.ca>
2015-11-26 13:50:09 -0800

    more elixiry

--- lib/luhn.ex
+++ lib/luhn.ex
@@ -1,13 +1,20 @@
 defmodule Luhn do
   def test(digits) do
     digits
-      |> to_char_list
-      |> Enum.reverse
-      |> Enum.map(&(&1-?0))
+      |> sanitize
       |> luhn_sum
       |> check
   end
- 
+  
+  defp sanitize(digits) do
+    digits
+      |> to_char_list
+      |> Enum.reverse
+      |> string_to_numbers
+  end
+  defp string_to_numbers(digits) do
+    Enum.map(digits, fn(digit) -> digit - ?0 end)
+  end
   defp luhn_sum([odd, even | rest]) when even >= 5, do:
       odd + 2 * even - 10 + 1 + luhn_sum(rest)
   defp luhn_sum([odd, even | rest]), do:
--- test/luhn_test.exs
+++ test/luhn_test.exs
@@ -2,7 +2,10 @@ defmodule LuhnTest do
   use ExUnit.Case
   doctest Luhn
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "valid number" do
+    assert Luhn.test(49927398716) == :valid
+  end
+  test "invalid number" do
+    assert Luhn.test(49927398717) == :invalid
   end
 end

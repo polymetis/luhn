90f560eea986aa6e65c8859eaac2fb54c4434c3c
Ley Missailidis <moisis.langley@missailidis.ca>
2015-11-26 12:55:24 -0800

    Didn't write this.
    Didn't trust my math skills. Taken from
    http://rosettacode.org/wiki/Luhn_test_of_credit_card_numbers#Elixir

---
+++ lib/luhn.ex
@@ -0,0 +1,15 @@
+defmodule Luhn do
+  def test(digits) do
+    to_char_list(digits) |> Enum.reverse |> Enum.map(&(&1-?0)) |> luhn_sum |> check
+  end
+ 
+  defp luhn_sum([odd, even | rest]) when even >= 5, do:
+      odd + 2 * even - 10 + 1 + luhn_sum(rest)
+  defp luhn_sum([odd, even | rest]), do:
+      odd + 2 * even + luhn_sum(rest)
+  defp luhn_sum([odd]), do: odd
+  defp luhn_sum([]), do: 0
+ 
+  defp check(sum) when rem(sum,10)==0, do: :valid
+  defp check(_sum), do: :invalid
+end
